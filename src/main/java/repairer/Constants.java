a6c411121df354a232f3cb408030d93c81d59f0e
Étienne <etienne@wattebled.fr>
2016-01-12 11:05:13 +0100

    màj

---
+++ src/main/java/repairer/Constants.java
@@ -0,0 +1,8 @@
+package repairer;
+
+public class Constants {
+	public static String SRC_CLASSES_PATH = "src\\main\\java\\";
+	public static String SRC_TESTCLASSES_PATH = "src\\test\\java";
+	public static String TARGET_CLASSES_PATH = "target\\classes\\";
+	public static String TARGET_TESTCLASSES_PATH = "target\\test-classes\\";
+}
