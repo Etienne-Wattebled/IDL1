19af5a2f767a77eb1563d80f088eb734fe855661
Étienne <etienne@wattebled.fr>
2016-01-16 19:20:25 +0100

    test

--- src/main/java/repairer/Repairer.java
+++ src/main/java/repairer/Repairer.java
@@ -89,7 +89,7 @@ public class Repairer {
 				dir = f.getParentFile();
 				url = dir.toURI().toURL();
 				ucl = new URLClassLoader(new URL[] { url });
-				classes[i] = ucl.loadClass(PathUtils.getPathAfter(PathUtils.getPathWithoutExtension(f.getAbsolutePath()),Constants.TARGET_TESTCLASSES_PATH).replaceAll(".class","").replaceAll("/",".").replaceAll("\\","."));
+				classes[i] = ucl.loadClass(PathUtils.getPathAfter(PathUtils.getPathWithoutExtension(f.getAbsolutePath()),Constants.TARGET_TESTCLASSES_PATH).replaceAll("/",".").replaceAll("\\","."));
 				i = i+1;
 			} catch (ClassNotFoundException cnfe) {
 				cnfe.printStackTrace();
